7e4a5d6b4e358126c72740f1bc12ea877563005c
Sam-NorthDev <smn.azm@gmail.com>
2025-12-23 00:50:51 -0700

    Update SysCheck.bat

--- SysCheck.bat
+++ SysCheck.bat
@@ -1,5 +1,27 @@
 @echo off
-echo Testing Sam-NorthDev Script...
-systeminfo > "%userprofile%\Desktop\Report.txt"
-echo Done! Look at your Desktop.
+title Sam-NorthDev IT Scanner
+echo ========================================
+echo   SYSTEM SCANNER BY SAM-NORTHDEV
+echo ========================================
+echo.
+echo Scanning... Please wait a few seconds...
+
+:: 1. System Information
+echo [SYSTEM INFO] > "%userprofile%\Desktop\Report.txt"
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"System Model" /C:"Total Physical Memory" >> "%userprofile%\Desktop\Report.txt"
+
+:: 2. BIOS Serial Number
+echo. >> "%userprofile%\Desktop\Report.txt"
+echo [BIOS SERIAL] >> "%userprofile%\Desktop\Report.txt"
+wmic bios get serialnumber >> "%userprofile%\Desktop\Report.txt"
+
+:: 3. Network Configuration
+echo. >> "%userprofile%\Desktop\Report.txt"
+echo [NETWORK INFO] >> "%userprofile%\Desktop\Report.txt"
+ipconfig /all >> "%userprofile%\Desktop\Report.txt"
+
+echo.
+echo ========================================
+echo DONE! Check 'Report.txt' on your Desktop.
+echo ========================================
 pause
